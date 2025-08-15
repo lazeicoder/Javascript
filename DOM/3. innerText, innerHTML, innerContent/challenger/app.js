8bc00a3675c30dae485318de28d2492b2f600d9c
lazeicoder <anuragmukherjeedev@gmail.com>
2025-08-15 20:28:35 +0530

    Added challenger of part 3 of DOM

---
+++ DOM/3. innerText, innerHTML, innerContent/challenger/app.js
@@ -0,0 +1,14 @@
+// 1. Check the innerText of "first" element
+
+const firstElement = document.querySelector('.first');
+console.log(firstElement.innerText);
+
+// 2. Check the textContent of "second" element
+
+const secondElement = document.querySelector('.second');
+console.log(secondElement.textContent);
+
+// 3. Check the innerHTML of "third" element
+
+const thirdElement = document.querySelector('.third');
+console.log(thirdElement.innerHTML);
